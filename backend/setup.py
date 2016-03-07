9d0fa58a2dc547faf70cb27399acc4cc53c2ca7b
Kelvin Lu <kelvinlu@ucdavis.edu>
2016-03-07 14:22:16 -0800

    add pytest to requirements, add setup.py script

---
+++ backend/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='Quadcopter/backend',
+    version='0.0.0'
+    description='Backend services for the Quadcopter project',
+    url='https://github.com/quadsquad193/Quadcopter',
+
+    setup_requires=['pytest-runner'],
+    test_requires=['pytest'],
+
+    packages=['droneservice', 'openalprservice', 'parkinglogservice', 'webservice'],
+    package_dir={
+        'droneservice': 'droneservice',
+        'openalprservice': 'openalprservice',
+        'parkinglogservice': 'parkinglogservice',
+        'webservice': 'webservice'
+    }
+)
