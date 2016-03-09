443f72c1b0d6a0fda53753b888fdc2d5f7e169c1
mhmachado <mhmachado@ucdavis.edu>
2016-03-08 23:31:03 -0800

    Set up simple single page website based off of last year's site.  All captures are shown on index.  Changes needed after demo.

--- core/phantomboreas/webservice/__init__.py
+++ core/phantomboreas/webservice/__init__.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import redis
-
-
+import config
+from db.models import Base
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+db_conf = {
+    'sqlite_url':   config.SQLALCHEMY_DATABASE_URI
+ }
+
+
+db_session = sessionmaker()
+db_engine = create_engine(db_conf['sqlite_url'])
+db_session.configure(bind=db_engine)
+Base.metadata.create_all(db_engine)
+
 
 redis_client = redis.StrictRedis(
     host=app.config['REDIS_CONN']['host'],
--- core/phantomboreas/webservice/views.py
+++ core/phantomboreas/webservice/views.py
@@ -2,10 +2,26 @@ from flask import request
 from flask.views import MethodView
 from flask import render_template
 
-import process
+from base64 import b64encode
 
+import process
+from phantomboreas.db.models import Base, CaptureLog, PlateLog, CandidateLog
+
+# import sqlalchemy as sa
+# from sqlalchemy import orm
+# from sqlalchemy import create_engine
+
+from phantomboreas.webservice import db_session
 
 
 class IndexView(MethodView):
     def get(self):
-        return render_template('index.html'), 200
+    	session = db_session()
+
+    	_capture_list = session.query(CaptureLog).all()
+    	capture_list = []
+
+    	for capture in _capture_list:
+    		capture_list.append({'filename': capture.filename, 'image': b64encode(capture.image)})
+
+        return render_template('index.html', capture_list=capture_list), 200
