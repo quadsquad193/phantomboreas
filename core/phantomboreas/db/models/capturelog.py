c553d2a15b65f99a33880ecc7fecdc64c6c4376d
mhmachado <mhmachado@ucdavis.edu>
2016-04-10 19:17:41 -0700

    Changed latiude and longitude in capture_log to fixed point numerics.

--- core/phantomboreas/db/models/capturelog.py
+++ core/phantomboreas/db/models/capturelog.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, LargeBinary, String, Float, DateTime
+from sqlalchemy import Column, Integer, LargeBinary, String, Float, DateTime, Numeric
 from sqlalchemy.dialects.mysql import MEDIUMBLOB
 from sqlalchemy.orm import relationship
 
@@ -13,8 +13,8 @@ class CaptureLog(Base):
 
     filepath        = Column(String(256))
     filename        = Column(String(256))
-    latitude        = Column(Float)
-    longitude       = Column(Float)
+    latitude        = Column(Numeric(precision=10, scale=7))
+    longitude       = Column(Numeric(precision=10, scale=7))
     timestamp       = Column(DateTime)
 
     plates = relationship("PlateLog", back_populates="capture")
